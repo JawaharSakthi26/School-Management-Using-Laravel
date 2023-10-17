d81a7362deb1e7ad0c95089febc1593768488bf6
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-10-17 20:10:16 +0530

    Login page  - Middleware for role need to do

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole('Admin')) {
+            return redirect()->route('admin.index');
+        } elseif ($user->hasRole('Teacher')) {
+            return redirect()->route('teacher.index');
+        } elseif ($user->hasRole('Student')) {
+            return redirect()->route('student.index');
+        }
+
+        return redirect($this->redirectTo);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,10 @@
                     <div class="login-right">
                         <div class="login-right-wrap">
                             <h1>Welcome to Preskool</h1>
-                            <p class="account-subtitle">Need an account? <a href="register.html">Sign Up</a></p>
-                            <h2>Sign in</h2>
+                            <h2 class="text-info fs-6 text-uppercase">Please Sign in</h2>
 
                             <form action="{{ route('login') }}" method="POST" id="login-form">
                                 @csrf
-
                                 <div class="form-group">
                                     <label>Username <span class="login-danger">*</span></label>
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" placeholder="Email" autofocus>
@@ -38,15 +36,6 @@
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
-                                </div>
-                                <div class="forgotpass">
-                                    <div class="remember-me">
-                                        <label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
-                                            <input type="checkbox" name="radio">
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div>
-                                    <a href="forgot-password.html">Forgot Password?</a>
                                 </div>
                                 <div class="form-group">
                                     <button class="btn btn-primary btn-block" type="submit">Login</button>
