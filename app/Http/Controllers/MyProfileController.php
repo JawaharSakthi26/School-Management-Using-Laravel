b0d5bc382cb095df15774191c56edee17d296199
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-10-25 19:36:14 +0530

    Updation done - My profile page

--- app/Http/Controllers/MyProfileController.php
+++ app/Http/Controllers/MyProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -59,21 +60,42 @@ class MyProfileController extends Controller
                 return redirect()->back()->with('error', 'The old password is incorrect.');
             }
 
-            if ($request->filled('new_password')) {
-                // Update the password if a new password is provided
-                $user->update(['password' => Hash::make($request->new_password)]);
+            if ($request->filled('password')) {
+                $user->update(['password' => Hash::make($request->password)]);
             }
-        } else {
-            // Update user details
-            $user->update([
-                'name' => $request->name,
-                'email' => $request->email,
-            ]);
-        }
 
+        } else {
+            if ($user->hasRole('Admin')) {
+                $user->update([
+                    'name' => $request->name,
+                    'email' => $request->email
+                ]);
+            } elseif ($user->hasRole('Teacher')) {
+                $user->update([
+                    'name' => $request->name,
+                    'email' => $request->email
+                ]);
+
+                $dob = Carbon::createFromFormat('d-m-Y', $request->input('dob'))->format('Y-m-d');
+                $joining_date = Carbon::createFromFormat('d-m-Y', $request->input('joining_date'))->format('Y-m-d');
+
+                $user->teacher->update([
+                    'phone' => $request->input('phone'),
+                    'gender' => $request->input('gender'),
+                    'dob' => $dob,
+                    'joining_date' => $joining_date,
+                    'qualification' => $request->input('qualification'),
+                    'blood_group' => $request->input('blood_group'),
+                    'address' => $request->input('address'),
+                    'city' => $request->input('city'),
+                    'state' => $request->input('state'),
+                    'zip_code' => $request->input('zip_code'),
+                    'country' => $request->input('country'),
+                ]);
+            }
+        }
         return redirect()->route('my-profile.index')->with('message', 'Profile updated successfully.');
     }
-
 
     /**
      * Remove the specified resource from storage.
