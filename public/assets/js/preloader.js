a5121f94837d6861004a4bfc61a23eb96b64e04e
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-11-03 16:03:36 +0530

    UI > Implemented preloader

---
+++ public/assets/js/preloader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    setTimeout(function() {
+        var preloader = document.querySelector(".preloader");
+        preloader.style.display = "none";
+    }, 200);
+});
