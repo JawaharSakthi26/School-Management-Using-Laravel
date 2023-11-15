207ea64ceb9f5ec1881c5b3ec17593b65d9d77b2
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-11-15 19:05:45 +0530

    Fee page > Completed crud in Admin module

--- app/Http/Controllers/Admin/FeeController.php
+++ app/Http/Controllers/Admin/FeeController.php
@@ -7,7 +7,6 @@ use App\Http\Traits\RestControllerTrait;
 use App\Models\AddClass;
 use App\Models\Fee_detail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 
 class FeeController extends Controller
 {
@@ -17,20 +16,16 @@ class FeeController extends Controller
     public $folderPath = 'admin';
     public $viewPath = 'fees';
     public $routeName = 'add-fees';
-    // public $export = MyStudentsExport::class;
     public $message = 'Fees';
-    // public $dataTable = StudentDataTable::class;
 
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
         $classes = AddClass::where('status', '1')->get();
         return view('admin.fees.index', compact('classes'));
     }
 
-    public function fetchFees(Request $request) 
+    public function fetchFees(Request $request)
     {
         $classId = $request->input('class_id');
 
@@ -58,7 +53,7 @@ class FeeController extends Controller
         $fees = null;
 
         if ($id) {
-            $fees = Fee_detail::findOrFail($id); //need to modify according to create page requirement
+            $fees = Fee_detail::findOrFail($id);
         }
 
         return [
@@ -76,7 +71,7 @@ class FeeController extends Controller
             ->first();
 
         if ($existingRecord) {
-            return redirect()->route('add-fees.create')->with('error', 'This term fee is already assigned for the class');
+            return redirect()->route('add-fees.index')->with('error', 'This term fee is already assigned for the class');
         }
 
         Fee_detail::create($data);
@@ -84,35 +79,29 @@ class FeeController extends Controller
         return redirect()->route('add-fees.index')->with('message', 'Fees created successfully!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
-    }
+        $fees = Fee_detail::findOrFail($id);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $data = $request->all();
+
+        $existingRecord = Fee_detail::where('class_id', $data['class_id'])
+        ->where('term', $data['term'])
+        ->whereNotIn('id', [$id]) 
+        ->first();
+
+        if ($existingRecord) {
+            return redirect()->route('add-fees.index')->with('error', 'This term fee is already assigned for the class');
+        }
+
+        $fees->update([
+            'class_id' => $data['class_id'],
+            'term' => $data['term'],
+            'amount' => $data['amount'],
+            'due_date' => $data['due_date'],
+            'user_id' => $data['user_id'],
+        ]);
+
+        return redirect()->route('add-fees.index')->with('message', 'Fees updated successfully');
     }
 }
--- resources/views/admin/fees/create.blade.php
+++ resources/views/admin/fees/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.auth')
-@section('title', 'PreSkool | Add Event')
+@section('title', 'PreSkool | ' . (isset($selectLookups['fees']) ? 'Edit' : 'Add') . ' Fees')
 @section('content')
     <div class="page-wrapper">
         <div class="content container-fluid">
@@ -7,10 +7,10 @@
             <div class="page-header">
                 <div class="row align-items-center">
                     <div class="col">
-                        <h3 class="page-title">Add Fees</h3>
+                        <h3 class="page-title">{{ isset($selectLookups['fees']) ? 'Edit' : 'Add' }} Fees</h3>
                         <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('calendar') }}">Fees</a></li>
-                            <li class="breadcrumb-item active">Add Fees</li>
+                            <li class="breadcrumb-item"><a href="{{ route('add-fees.index') }}">Fees</a></li>
+                            <li class="breadcrumb-item active">{{ isset($selectLookups['fees']) ? 'Edit' : 'Add' }} Fees</li>
                         </ul>
                     </div>
                 </div>
@@ -19,11 +19,13 @@
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-body">
-                            <form action="{{ route('add-fees.store') }}" method="POST" id="fees-form">
+                            <form action="{{ isset($selectLookups['fees']) ? route('add-fees.update', $selectLookups['fees']->id) : route('add-fees.store') }}"
+                                method="POST" id="fees-form">
                                 @csrf
+                                {{ isset($selectLookups['fees']) ? method_field('PUT') : '' }}
                                 <div class="row">
                                     <div class="col-12">
-                                        <h5 class="form-title"><span>Add Fees</span></h5>
+                                        <h5 class="form-title"><span>{{ isset($selectLookups['fees']) ? 'Edit' : 'Add' }} Fees</span></h5>
                                     </div>
                                     <div class="col-12 col-sm-6">
                                         <div class="form-group local-forms">
@@ -31,7 +33,9 @@
                                             <select class="form-control select" name="class_id" id="class_id">
                                                 <option value="">-- Select Class --</option>
                                                 @foreach ($selectLookups['classes'] as $class)
-                                                    <option value="{{ $class->id }}">{{ $class->name }}</option>
+                                                    <option value="{{ $class->id }}"
+                                                        {{ old('class_id', isset($selectLookups['fees']) && $selectLookups['fees']->class_id == $class->id ? 'selected' : '') }}>
+                                                        {{ $class->name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
@@ -42,7 +46,9 @@
                                             <select class="form-control select" name="term" id="term">
                                                 <option value="">-- Select Term --</option>
                                                 @foreach (config('custom.school_terms') as $key => $terms)
-                                                    <option value="{{ $key }}">{{ $terms }}</option>
+                                                    <option value="{{ $key }}"
+                                                        {{ old('term', isset($selectLookups['fees']) && $selectLookups['fees']->term == $key ? 'selected' : '') }}>
+                                                        {{ $terms }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
@@ -51,21 +57,24 @@
                                     <div class="col-12 col-sm-6">
                                         <div class="form-group local-forms">
                                             <label>Fees Amount <span class="login-danger">*</span></label>
-                                            <input class="form-control" id="money" type="text" name="amount">
+                                            <input class="form-control" id="money" type="text" name="amount"
+                                                value="{{ old('amount', isset($selectLookups['fees']) ? $selectLookups['fees']->amount : '') }}">
                                         </div>
                                     </div>
 
                                     <div class="col-12 col-sm-6">
                                         <div class="form-group local-forms">
                                             <label>Due Date <span class="login-danger">*</span></label>
-                                            <input type="date" name="due_date" class="form-control">
+                                            <input type="date" name="due_date" class="form-control"
+                                                value="{{ old('due_date', isset($selectLookups['fees']) ? $selectLookups['fees']->due_date : '') }}">
                                         </div>
                                     </div>
 
                                     <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                                     <div class="col-12">
                                         <div class="student-submit">
-                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                            <button type="submit"
+                                                class="btn btn-primary">{{ isset($selectLookups['fees']) ? 'Update' : 'Submit' }}</button>
                                         </div>
                                     </div>
                                 </div>
