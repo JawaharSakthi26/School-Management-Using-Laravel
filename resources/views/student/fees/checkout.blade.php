f9cd69ed77acad6d34a38da3f32f13041e518eef
Jawahar <jawaharsakthi.v@arkinfotec.com>
2023-11-22 12:27:10 +0530

    Checkout: Payment issue yet to be completed

--- resources/views/student/fees/checkout.blade.php
+++ resources/views/student/fees/checkout.blade.php
@@ -63,71 +63,76 @@
 
     <script src="https://js.stripe.com/v3/"></script>
     <script>
-        var stripe = Stripe('{{ env('STRIPE_KEY') }}');
-        var elements = stripe.elements();
-        var style = {
-            base: {
-                color: '#32325d',
-                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                fontSmoothing: 'antialiased',
-                fontSize: '16px',
-                '::placeholder': {
-                    color: '#aab7c4'
+        $(document).ready(function() {
+            var stripe = Stripe('{{ env('STRIPE_KEY') }}');
+            var elements = stripe.elements();
+            var style = {
+                base: {
+                    color: '#32325d',
+                    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                    fontSmoothing: 'antialiased',
+                    fontSize: '16px',
+                    '::placeholder': {
+                        color: '#aab7c4'
+                    }
+                },
+                invalid: {
+                    color: '#fa755a',
+                    iconColor: '#fa755a'
                 }
-            },
-            invalid: {
-                color: '#fa755a',
-                iconColor: '#fa755a'
-            }
-        };
-        var card = elements.create('card', {
-            hidePostalCode: true,
-            style: style
-        });
-        card.mount('#card-element');
-        card.addEventListener('change', function(event) {
-            var displayError = document.getElementById('card-errors');
-            if (event.error) {
-                displayError.textContent = event.error.message;
-            } else {
-                displayError.textContent = '';
-            }
-        });
-        const cardHolderName = document.getElementById('card-holder-name');
-        const cardButton = document.getElementById('card-button');
-        const clientSecret = cardButton.dataset.secret;
-        cardButton.addEventListener('click', async (e) => {
-            e.preventDefault();
-            console.log("attempting");
-            const {
-                setupIntent,
-                error
-            } = await stripe.confirmCardSetup(
-                clientSecret, {
+            };
+
+            var card = elements.create('card', {
+                hidePostalCode: true,
+                style: style
+            });
+
+            card.mount('#card-element');
+            card.on('change', function(event) {
+                var displayError = $('#card-errors');
+                if (event.error) {
+                    displayError.text(event.error.message);
+                } else {
+                    displayError.text('');
+                }
+            });
+
+            const cardHolderName = $('#card-holder-name');
+            const cardButton = $('#card-button');
+            const clientSecret = cardButton.data('secret');
+
+            cardButton.on('click', async function(e) {
+                e.preventDefault();
+                console.log("attempting");
+                const {
+                    setupIntent,
+                    error
+                } = await stripe.confirmCardSetup(clientSecret, {
                     payment_method: {
                         card: card,
                         billing_details: {
-                            name: cardHolderName.value
+                            name: cardHolderName.val()
                         }
                     }
+                });
+
+                if (error) {
+                    var errorElement = $('#card-errors');
+                    errorElement.text(error.message);
+                } else {
+                    paymentMethodHandler(setupIntent.payment_method);
                 }
-            );
-            if (error) {
-                var errorElement = document.getElementById('card-errors');
-                errorElement.textContent = error.message;
-            } else {
-                paymentMethodHandler(setupIntent.payment_method);
+            });
+
+            function paymentMethodHandler(payment_method) {
+                var form = $('#subscribe-form');
+                var hiddenInput = $('<input>')
+                    .attr('type', 'hidden')
+                    .attr('name', 'payment_method')
+                    .attr('value', payment_method);
+                form.append(hiddenInput);
+                form.submit();
             }
         });
-
-        function paymentMethodHandler(payment_method) {
-            var form = document.getElementById('subscribe-form');
-            var hiddenInput = document.createElement('input');
-            hiddenInput.setAttribute('type', 'hidden');
-            hiddenInput.setAttribute('name', 'payment_method');
-            hiddenInput.setAttribute('value', payment_method);
-            form.appendChild(hiddenInput);
-            form.submit();
-        }
     </script>
 @endsection
